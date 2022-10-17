1201efebb4da14e1aaf987748f0d49a671549c25
aibayanyu20 <aibayanyu@qq.com>
2022-10-17 09:28:57 +0800

    chore(docs): change parser

--- src/parser/md-parser.ts
+++ src/parser/md-parser.ts
@@ -1,9 +1,5 @@
-import path from 'path'
-import fs from 'fs/promises'
 import { load } from 'cheerio'
-// @ts-expect-error no types
-import lunr from '../lunr.esm'
 export const mdParser = async(html: string, env: Record<string, any>) => {
   const $ = load(html)
-  console.log($)
+  // console.log($('.header-anchor').find('h'))
 }
